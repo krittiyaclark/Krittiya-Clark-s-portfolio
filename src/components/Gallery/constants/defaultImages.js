0d7bf853768be35c4a651caa79f540252c9974e8
krittiyaclark <krittiyaclark@gmail.com>
2023-06-04 23:32:56 -0400

    Removed Node-Sass and installed Sass, updated portfolio

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -1,23 +1,28 @@
 import full01 from '../../../assets/images/fulls/ViiHost-Soicial-Network-Web-Applicaton.png'
 import full02 from '../../../assets/images/fulls/Kissane-Associates-image.png'
 import full03 from '../../../assets/images/fulls/Pho-Thai-Express-Restaurant.png'
-import full04 from '../../../assets/images/fulls/Today-Plan-Full-Stack-App.png'
+import full05 from '../../../assets/images/fulls/Pixel-Art-Project.png'
+import full06 from '../../../assets/images/fulls/React Beach Resort.png'
+import full07 from '../../../assets/images/fulls/Memory-Game.png'
 
 import thumb01 from '../../../assets/images/thumbs/ViiHost-Soicial-Network-Web-Applicaton.png'
 import thumb02 from '../../../assets/images/thumbs/Kissane-Associates-image.png'
 import thumb03 from '../../../assets/images/thumbs/Pho-Thai-Express-Restaurant.png'
-import thumb04 from '../../../assets/images/thumbs/Today-Plan-Full-Stack-App.png'
+import thumb05 from '../../../assets/images/thumbs/Pixel-Art-Project.png'
+import thumb06 from '../../../assets/images/thumbs/React Beach Resort.png'
+import thumb07 from '../../../assets/images/thumbs/Memory-Game.png'
+
+import React from 'react'
 
 export const DEFAULT_IMAGES = [
   {
     id: '1',
-    source: full04,
-    thumbnail: thumb04,
-    caption: 'Today Plan - Todo Plan Full Stack App',
-    technology: 'Node, MongoDB, Express, JavaScript, Azure, Bootstrap',
+    source: full05,
+    thumbnail: thumb05,
+    caption: 'Pixel Art Project',
     description:
-      "This application lets you easily add and delete your own authentication plans with Microsoft Azure's authentication system.",
-    url: 'https://today-plan-fullstack-app.herokuapp.com',
+      'Pixel Art Maker JavaScript application. Creating pixels involves building grids and then coloring the grids in order to create individual pixels.',
+    url: 'https://krittiyaclark.github.io/udacity-project-pixel-art-maker-vanila-JS',
   },
   {
     id: '2',
@@ -47,28 +52,22 @@ export const DEFAULT_IMAGES = [
     technology: 'Gatsby, CSS-Grid, Contentful, Netlify',
     url: 'https://www.phothaiexpress.com',
   },
-  // {
-  //   id: '4',
-  //   source: full04,
-  //   thumbnail: thumb04,
-  //   caption: 'Photo 4',
-  //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  //   url: 'https://social-network-638c8.firebaseapp.com',
-  // },
-  // {
-  //   id: '5',
-  //   source: full05,
-  //   thumbnail: thumb05,
-  //   caption: 'Photo 5',
-  //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  //   url: 'https://social-network-638c8.firebaseapp.com',
-  // },
-  // {
-  //   id: '6',
-  //   source: full06,
-  //   thumbnail: thumb06,
-  //   caption: 'Photo 6',
-  //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  //   url: 'https://social-network-638c8.firebaseapp.com',
-  // },
+  {
+    id: '5',
+    source: full06,
+    thumbnail: thumb06,
+    caption: 'React Beach Resort',
+    description:
+      'This is a responsive website built with React. It is a website that provides information about a beach resort, including room specifics and features for sorting. arranged in order of room type, price, features, and maximum occupancy.',
+    url: 'https://beach-resort-react-app-live.netlify.app',
+  },
+  {
+    id: '6',
+    source: full07,
+    thumbnail: thumb07,
+    caption: 'Memory-Game',
+    description:
+      "Memory A game application was developed to check the user's level of memory. There is a deck of cards that features a variety of symbols.<br />Instructions<br /To select a card, just click on it. You'll be able to match cards in less time and with fewer moves. Rating out of stars: starts off at 3, then drops to 2 after 15 moves, and finally drops to 1 after more than 30 moves.",
+    url: 'http://threatening-mass.surge.sh',
+  },
 ]
--- src/components/Gallery/constants/dist/defaultImages.dev.js
+++ src/components/Gallery/constants/dist/defaultImages.dev.js
@@ -1,83 +1,107 @@
-"use strict";
+'use strict'
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.DEFAULT_IMAGES = void 0;
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+})
+exports.DEFAULT_IMAGES = void 0
 
-var _ViiHostSoicialNetworkWebApplicaton = _interopRequireDefault(require("../../../assets/images/fulls/ViiHost-Soicial-Network-Web-Applicaton.png"));
+var _ViiHostSoicialNetworkWebApplicaton = _interopRequireDefault(
+  require('../../../assets/images/fulls/ViiHost-Soicial-Network-Web-Applicaton.png')
+)
 
-var _KissaneAssociatesImage = _interopRequireDefault(require("../../../assets/images/fulls/Kissane-Associates-image.png"));
+var _KissaneAssociatesImage = _interopRequireDefault(
+  require('../../../assets/images/fulls/Kissane-Associates-image.png')
+)
 
-var _PhoThaiExpressRestaurant = _interopRequireDefault(require("../../../assets/images/fulls/Pho-Thai-Express-Restaurant.png"));
+var _PhoThaiExpressRestaurant = _interopRequireDefault(
+  require('../../../assets/images/fulls/Pho-Thai-Express-Restaurant.png')
+)
 
-var _TodayPlanFullStackApp = _interopRequireDefault(require("../../../assets/images/fulls/Today-Plan-Full-Stack-App.png"));
+var _TodayPlanFullStackApp = _interopRequireDefault(
+  require('../../../assets/images/fulls/Today-Plan-Full-Stack-App.png')
+)
 
-var _ViiHostSoicialNetworkWebApplicaton2 = _interopRequireDefault(require("../../../assets/images/thumbs/ViiHost-Soicial-Network-Web-Applicaton.png"));
+var _ViiHostSoicialNetworkWebApplicaton2 = _interopRequireDefault(
+  require('../../../assets/images/thumbs/ViiHost-Soicial-Network-Web-Applicaton.png')
+)
 
-var _KissaneAssociatesImage2 = _interopRequireDefault(require("../../../assets/images/thumbs/Kissane-Associates-image.png"));
+var _KissaneAssociatesImage2 = _interopRequireDefault(
+  require('../../../assets/images/thumbs/Kissane-Associates-image.png')
+)
 
-var _PhoThaiExpressRestaurant2 = _interopRequireDefault(require("../../../assets/images/thumbs/Pho-Thai-Express-Restaurant.png"));
+var _PhoThaiExpressRestaurant2 = _interopRequireDefault(
+  require('../../../assets/images/thumbs/Pho-Thai-Express-Restaurant.png')
+)
 
-var _TodayPlanFullStackApp2 = _interopRequireDefault(require("../../../assets/images/thumbs/Today-Plan-Full-Stack-App.png"));
+var _TodayPlanFullStackApp2 = _interopRequireDefault(
+  require('../../../assets/images/thumbs/Today-Plan-Full-Stack-App.png')
+)
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
 
-var DEFAULT_IMAGES = [{
-  id: '1',
-  source: _TodayPlanFullStackApp["default"],
-  thumbnail: _TodayPlanFullStackApp2["default"],
-  caption: 'Today Plan - Todo Plan Full Stack App',
-  technology: 'Node, MongoDB, Express, JavaScript, Azure, Bootstrap',
-  description: "This application lets you easily add and delete your own authentication plans with Microsoft Azure's authentication system.",
-  url: 'https://today-plan-fullstack-app.herokuapp.com'
-}, {
-  id: '2',
-  source: _ViiHostSoicialNetworkWebApplicaton["default"],
-  thumbnail: _ViiHostSoicialNetworkWebApplicaton2["default"],
-  caption: 'ViiHost - Social Network',
-  technology: 'React, Semantic UI, Firebase & Redux',
-  description: 'Social network web application where the user can create an event based on categories, and the users can join and chat in the event. Just like all other social networks, the user can follow other users.',
-  url: 'https://social-network-638c8.firebaseapp.com'
-}, {
-  id: '3',
-  source: _KissaneAssociatesImage["default"],
-  thumbnail: _KissaneAssociatesImage2["default"],
-  caption: 'Kissane Associates',
-  description: 'Law firm in St. Albans, Vermont',
-  technology: 'Gatsby, React-Bootstrap, DatoCMS, Netlify',
-  url: 'https://www.kissaneassociates.com'
-}, {
-  id: '4',
-  source: _PhoThaiExpressRestaurant["default"],
-  thumbnail: _PhoThaiExpressRestaurant2["default"],
-  caption: 'Pho Thai Express',
-  description: 'Authentic Thai Restaurant in Montpelier, Vermont',
-  technology: 'Gatsby, CSS-Grid, Contentful, Netlify',
-  url: 'https://www.phothaiexpress.com'
-} // {
-//   id: '4',
-//   source: full04,
-//   thumbnail: thumb04,
-//   caption: 'Photo 4',
-//   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-//   url: 'https://social-network-638c8.firebaseapp.com',
-// },
-// {
-//   id: '5',
-//   source: full05,
-//   thumbnail: thumb05,
-//   caption: 'Photo 5',
-//   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-//   url: 'https://social-network-638c8.firebaseapp.com',
-// },
-// {
-//   id: '6',
-//   source: full06,
-//   thumbnail: thumb06,
-//   caption: 'Photo 6',
-//   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-//   url: 'https://social-network-638c8.firebaseapp.com',
-// },
-];
-exports.DEFAULT_IMAGES = DEFAULT_IMAGES;
+var DEFAULT_IMAGES = [
+  {
+    id: '1',
+    source: _TodayPlanFullStackApp['default'],
+    thumbnail: _TodayPlanFullStackApp2['default'],
+    caption: 'Today Plan - Todo Plan Full Stack App',
+    technology: 'Node, MongoDB, Express, JavaScript, Azure, Bootstrap',
+    description:
+      "This application lets you easily add and delete your own authentication plans with Microsoft Azure's authentication system.",
+    url: 'https://today-plan-fullstack-app.herokuapp.com',
+  },
+  {
+    id: '2',
+    source: _ViiHostSoicialNetworkWebApplicaton['default'],
+    thumbnail: _ViiHostSoicialNetworkWebApplicaton2['default'],
+    caption: 'ViiHost - Social Network',
+    technology: 'React, Semantic UI, Firebase & Redux',
+    description:
+      'Social network web application where the user can create an event based on categories, and the users can join and chat in the event. Just like all other social networks, the user can follow other users.',
+    url: 'https://social-network-638c8.firebaseapp.com',
+  },
+  {
+    id: '3',
+    source: _KissaneAssociatesImage['default'],
+    thumbnail: _KissaneAssociatesImage2['default'],
+    caption: 'Kissane Associates',
+    description: 'Law firm in St. Albans, Vermont',
+    technology: 'Gatsby, React-Bootstrap, DatoCMS, Netlify',
+    url: 'https://www.kissaneassociates.com',
+  },
+  {
+    id: '4',
+    source: _PhoThaiExpressRestaurant['default'],
+    thumbnail: _PhoThaiExpressRestaurant2['default'],
+    caption: 'Pho Thai Express',
+    description: 'Authentic Thai Restaurant in Montpelier, Vermont',
+    technology: 'Gatsby, CSS-Grid, Contentful, Netlify',
+    url: 'https://www.phothaiexpress.com',
+  }, // {
+  //   id: '4',
+  //   source: full04,
+  //   thumbnail: thumb04,
+  //   caption: 'Photo 4',
+  //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+  //   url: 'https://social-network-638c8.firebaseapp.com',
+  // },
+  // {
+  //   id: '5',
+  //   source: full05,
+  //   thumbnail: thumb05,
+  //   caption: 'Photo 5',
+  //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+  //   url: 'https://social-network-638c8.firebaseapp.com',
+  // },
+  // {
+  //   id: '6',
+  //   source: full06,
+  //   thumbnail: thumb06,
+  //   caption: 'Photo 6',
+  //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+  //   url: 'https://social-network-638c8.firebaseapp.com',
+  // },
+]
+exports.DEFAULT_IMAGES = DEFAULT_IMAGES
